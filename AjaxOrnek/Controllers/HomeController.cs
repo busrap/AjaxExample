ac2c4ffcf0d86096effe6c90080d96e7fe643dd8
busra06 <busra.polat.tr@gmail.com>
2021-01-06 13:20:22 +0300

    ajaxExample

---
+++ AjaxOrnek/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxOrnek.Controllers
+{
+    // AJAX: (Asynchronous Javascript and Xml) Sayfa yeniden yüklenmeden sunucudan bilgi alabilmeyi sağlayan bir programlama tekniğidir. Tüm sayfanın güncellenmesi yerine sadece istenilen yerlerin güncellenmesini sağlar...
+
+    // NuGet'ten Microsoft.jQuery.Unobtrusive.Ajax isimli paketi install etmemiz gerekiyor!..
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AjaxOrnek/Controllers/UrunlerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxOrnek.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        List<string> isimler = new List<string> { "Ankara", "İzmir", "İstanbul", "Kars", "Zonguldak", "Edirne", "Sivas", "Kayseri", "Ağrı", "Muğla", "Muş", "Antalya", "Eskişehir" };
+
+        [HttpPost]
+        public ActionResult UrunleriGetir(string ara)
+        {
+            System.Threading.Thread.Sleep(1000);
+            return PartialView(isimler.Where(i => i.Contains(ara)).ToList());
+        }
+
+        [HttpPost]
+        public ActionResult UrunleriGetirIleBaslayan(string ara)
+        {
+            System.Threading.Thread.Sleep(1000);
+            return PartialView("UrunleriGetir", isimler.Where(i => i.StartsWith(ara)).ToList());
+        }
+    }
+}
